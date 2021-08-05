64831c27026e6d11c192d708e209d6f92a7eba19
David D Wang <duendwang@users.noreply.github.com>
2021-08-04 20:02:29 -0700

    custom admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+
+    fieldsets = [
+        (None, {
+            'fields': [
+                'question_text'
+                ]}),
+        ('Date information', {
+            'fields': [
+                'pub_date'
+                ]}),
+    ]
+    inlines = [ChoiceInline]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
